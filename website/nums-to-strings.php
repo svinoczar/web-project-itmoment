471d064e837f762d8b8f471ff5ca73a2dab92f29
AntonSexov <antonvedernikov2003@gmail.com>
2023-03-22 02:59:24 +0300

    nums-to-strings.php redacted

--- website/nums-to-strings.php
+++ website/nums-to-strings.php
@@ -5,33 +5,30 @@ $link = mysqli_connect("db4free.net", "itmo_user", "mUhNf!JELM349ii", "itmoment"
 if (!$link) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$name = "knopkipoehali"; // !!ЭТО ДЛЯ ТЕСТА . В ЭТУ ПЕРЕМЕННУЮ НАДО ЗАГОНЯТЬ НАЗВАНИЕ ПРОФЕССИИ
+// $name = "knopkipoehali"; // !!ЭТО ДЛЯ ТЕСТА . В ЭТУ ПЕРЕМЕННУЮ НАДО ЗАГОНЯТЬ НАЗВАНИЕ ПРОФЕССИИ
 
-$query = "SELECT `pqs` FROM `profession_pqs` WHERE `profession_name` = '$name'";
-$result = mysqli_query($link, $query);
+// $query = "SELECT `pqs` FROM `profession_pqs` WHERE `profession_name` = '$name'";
+// $result = mysqli_query($link, $query);
 
-if (mysqli_num_rows($result) > 0) {
-  $row = mysqli_fetch_array($result);
-  $input = $row["pqs"];
-}
-
+// if (mysqli_num_rows($result) > 0) {
+//   $row = mysqli_fetch_array($result);
+//   $input = $row["pqs"];
+// }
+$input_arr = array(1, 2, 5, 65, 8);
 $names = array();
 
 
-for ($i = 0; $i < strlen($input); $i++) {
-  $digit = $input[$i]; // это вес, здесь только проверка на больше меньше нуля
+foreach ($input_arr as $input){
 
-  $id = $i + 1;
-  if ($id >= 0  && $digit!=0) {
-    $query = "SELECT `kind` FROM `PQ` WHERE `id` = '$id'";
-    $result = mysqli_query($link, $query);
+  $query = "SELECT `kind` FROM `PQ` WHERE `id` = '$input'";
+  $result = mysqli_query($link, $query);
     
-    if (mysqli_num_rows($result) > 0) {
+  if (mysqli_num_rows($result) > 0) {
 
-      $row = mysqli_fetch_array($result);
-      $names[] = $row["kind"];
-    }
+    $row = mysqli_fetch_array($result);
+    $names[] = $row["kind"];
   }
+
 }
 
 
