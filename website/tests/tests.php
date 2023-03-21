6afa002334d247e7790eebbb81373631f6cdaae7
artemijr <arrtemijr@gmail.com>
2023-03-21 17:23:26 +0300

    оформил страницу с тестами

--- website/tests/tests.php
+++ website/tests/tests.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="ru" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -12,4 +12,90 @@
     <a href="evenoddvoice.php">evenoddvoice</a>
     <a href="evenoddtext.php">evenoddtxt</a>
   </body>
-</html>
+</html> -->
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Cool Modern Design</title>
+    <style>
+      body {
+        font-family: Courier New, monospace;
+        background-color: #2f2f2f;
+        color: #e3e3e3;
+      }
+      .button-block {
+        display: flex;
+        align-items: center;
+        background-color: #2c2c2c;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        margin-bottom: 10px;
+        padding: 10px;
+      }
+      .button-block-description {
+        flex: 1;
+        margin-right: 20px;
+      }
+      .button-block-button {
+        background-color: #c92fe8;
+        border: none;
+        color: #fff;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      .button-block-button:hover {
+        background-color: #9c80f7;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="button-block">
+      <div class="button-block-description">
+        <h2>Test 1</h2>
+        <p>Description for Test 1 goes here.</p>
+      </div>
+      <a href="test1.php" class="button-block-button">Test 1</a>
+    </div>
+    <div class="button-block">
+      <div class="button-block-description">
+        <h2>Test Sound</h2>
+        <p>Description for Test Sound goes here.</p>
+      </div>
+      <a href="test_sound.php" class="button-block-button">Test Sound</a>
+    </div>
+    <div class="button-block">
+      <div class="button-block-description">
+        <h2>Test Color</h2>
+        <p>Description for Test Color goes here.</p>
+      </div>
+      <a href="test_color.php" class="button-block-button">Test Color</a>
+    </div>
+    <div class="button-block">
+      <div class="button-block-description">
+        <h2>Test Color</h2>
+        <p>Description for Test Color goes here.</p>
+      </div>
+      <a href="test_color3.php" class="button-block-button">Test Color3</a>
+    </div>
+    <div class="button-block">
+      <div class="button-block-description">
+        <h2>Test Color</h2>
+        <p>Description for Test Color goes here.</p>
+      </div>
+      <a href="evenoddvoice.php" class="button-block-button">evenoddvoice</a>
+    </div>
+    <div class="button-block">
+      <div class="button-block-description">
+        <h2>Test Color</h2>
+        <p>Description for Test Color goes here.</p>
+      </div>
+      <a href="evenoddtext.php" class="button-block-button">evenoddtext</a>
+    </div>
+      </body>
+    </html>
