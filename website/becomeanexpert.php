757138d6483916e68c35ebf86d02cbd9d96aa088
artemijr <arrtemijr@gmail.com>
2023-03-21 04:07:31 +0300

    страница стать экспертом(тоже доработать)

---
+++ website/becomeanexpert.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>чтобы стать экспертом пиши роме на телефон +79116535423</h1>
+  </body>
+</html>
